705c93e444e6cc61543c19977a717d72da841e97
EchoTheVee <taikorodon@gmail.com>
2024-12-10 09:56:58 -0800

    Added Ability to Grab Barrels

--- Assets/Scripts/GrabController.cs
+++ Assets/Scripts/GrabController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GrabController : MonoBehaviour
 {
+    public GameObject barrel;
+    public GameObject barrelHolder;
+    private bool canGrabBarrel = false;
+    private bool barrelHold = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,39 @@ public class GrabController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && canGrabBarrel)
+        {
+            barrelHold = true;
+        }
+
+        if (barrelHold)
+        {
+            barrel.transform.position = barrelHolder.transform.position;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && !canGrabBarrel)
+        {
+            barrelHold = false;
+        }
+    }
+
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrel") && !barrelHold)
+        {
+            barrel = other.gameObject;
+            canGrabBarrel = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Barrel") )
+        {
+            //barrel = null;
+            canGrabBarrel = false;
+        }
     }
 }
