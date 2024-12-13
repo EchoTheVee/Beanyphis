5e378a4abbdcb608152fefdbc7071bf87c3b02f5
EchoTheVee <taikorodon@gmail.com>
2024-12-13 09:42:01 -0800

    Added Crate Enemy Functionality

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Rigidbody crateRb;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        crateRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        crateRb.AddRelativeForce(Vector2.right * moveSpeed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            transform.Rotate(Vector3.up * 180);
+        }
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
