c40893b3d4a156128ad2e8e09fe9e3255024ee08
EchoTheVee <taikorodon@gmail.com>
2024-12-16 08:57:23 -0800

    Added Button Functionality

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Barrel"))
+        {
+            gameObject.SetActive(false);
+            Destroy(collision.gameObject);
+        }
+    }
+}
