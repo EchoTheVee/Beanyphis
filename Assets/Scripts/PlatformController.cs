3ff4cb5772e9518a6d81554883695578ea6811ab
EchoTheVee <taikorodon@gmail.com>
2024-12-20 09:33:34 -0800

    Did Level Design, Added Moving Platforms, Added More Collectables

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlatformController : MonoBehaviour
 {
+    public GameObject _targetA;
+    public GameObject _targetB;
+    public GameObject _currentTarget; // = _targetA;
+    public float _speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,18 @@ public class PlatformController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float x_movemen
+        float distanceToA = Vector3.Distance(transform.position, _targetA.transform.position);
+        float distanceToB = Vector3.Distance(transform.position, _targetB.transform.position);
+
+        if (distanceToA == 0f)
+        {
+            _currentTarget = _targetB;
+        }
+
+        if (distanceToB == 0f)
+        {
+            _currentTarget = _targetA;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, _currentTarget.transform.position, _speed * Time.deltaTime);
     }
 }
